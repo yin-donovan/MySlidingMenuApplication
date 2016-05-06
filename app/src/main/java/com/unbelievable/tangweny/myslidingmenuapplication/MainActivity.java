6be20d9fa197a860ef5b3043114c11539e7c4956
tangweny <864084610@qq.com>
2016-05-06 17:41:29 +0800

    Initial commit

---
+++ app/src/main/java/com/unbelievable/tangweny/myslidingmenuapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.unbelievable.tangweny.myslidingmenuapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
